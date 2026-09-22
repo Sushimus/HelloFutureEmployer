9c61a3f8116bdc2b13388b8b999a31fb8199b469
sushimus <59678594+Sushimus@users.noreply.github.com>
2026-09-22 16:12:58 -0500

    i love brew

---
+++ main.cs
@@ -0,0 +1,31 @@
+using System;
+
+class Program {
+    static void Main() {
+        Console.WriteLine("Brew walk outside. Sun hot.");
+        Console.WriteLine("Brew see small puddle. Look inside.");
+        Console.WriteLine("No fish. Just mud.");
+        Console.WriteLine("Brew walk to big green pond.");
+        Console.WriteLine("Water sit still. Bugs fly on top.");
+        Console.WriteLine("Brew see orange flash. Big koi fish swim.");
+        Console.WriteLine("Brew try touch. Splash! Paw wet.");
+        Console.WriteLine("Koi swim deep. Hide from Brew.");
+        Console.WriteLine("Brew walk to river. Water move fast.");
+        Console.WriteLine("River make loud noise.");
+        Console.WriteLine("Brew see silver fish jump.");
+        Console.WriteLine("Silver fish fast. Too fast for paw.");
+        Console.WriteLine("Brew walk to ocean beach.");
+        Console.WriteLine("Water taste bad. Very salty.");
+        Console.WriteLine("Waves crash big. Brew scared of ocean.");
+        Console.WriteLine("Many fish in ocean, but too big for Brew.");
+        Console.WriteLine("Brew go home. Walk through cat door.");
+        Console.WriteLine("House quiet. Hear strange noise.");
+        Console.WriteLine("Drip. Drop. Splash.");
+        Console.WriteLine("Brew jump on bathroom counter.");
+        Console.WriteLine("Sink running. Clear water fall down hole.");
+        Console.WriteLine("Brew stare down drain.");
+        Console.WriteLine("Brew wonder what fish live in shiny silver hole.");
+        Console.WriteLine("Maybe small fish. Maybe fast fish.");
+        Console.WriteLine("Brew wait. Watch hole. Wait for secret sink fish.");
+    }
+}
